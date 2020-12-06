ef54d87a837fe6275c05ebaa3718acc2179f70b6
Victor <victor.lecardonnel@hotmail.fr>
2020-12-07 00:29:08 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+
+	p := readFile(getInput(0), getInput(1))
+	p.dumpToFile("test.txt")
+
+}
---
+++ plist.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+type plist []string
+
+func getInput(marker int) string {
+	reader := bufio.NewReader(os.Stdin)
+	if marker == 0 {
+		fmt.Print("Please enter the name of the file in which your proxies are located (e.g. proxies.txt): ")
+		input, _ := reader.ReadString('\n')
+		return strings.TrimSpace(input)
+	} else if marker == 1 {
+		fmt.Print("Please enter the range which you want to filter (e.g. proxies.txt): ")
+		input, _ := reader.ReadString('\n')
+		return strings.TrimSpace(input)
+	} else {
+		return ("proxy.txt")
+	}
+}
+
+func readFile(filename string, d string) plist {
+
+	l := plist{}
+
+	file, err := os.Open(filename)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if strings.Split(scanner.Text(), ".")[0] != d {
+			l = append(l, scanner.Text())
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return plist(l)
+}
+
+func (l plist) dumpToFile(filename string) {
+	file, err := os.Create(filename)
+
+	w := bufio.NewWriter(file)
+
+	for _, s := range l {
+		w.WriteString(s + "\n")
+	}
+
+	w.Flush()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
